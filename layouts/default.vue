3d1a88956deaaf8c2b3df4176f92442a9c33447a
Magicman <nikita_balashov_2005@mail.ru>
2025-03-17 15:31:36 +0500

    1111

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,75 +1,81 @@
 <template>
-    <header class= "flex flex-row w-full shadow-lg shadow-gray-400 max-sm:relative max-sm:justify-between">
+    <header class="flex flex-row w-full shadow-lg shadow-gray-400 max-sm:relative max-sm:justify-between">
         <div class="basis-1/4 items-center justify-start px-4 max-sm:basis-auto">
-            <img src="assets/images/skull.svg" class = "w-14 h-16">
+            <img src="assets/images/skull.svg" class="w-14 h-16" alt="Logo">
         </div>
-        <div class="basis-1/4 max-sm:basis-auto">
-        </div>
-        <nav class="{'basis-1/2 flex flex-row items-center justify-end px-8 gap-4': !burger || burger,
-        'masx-sm:basis-auto max-sm:absolute max-sm:top-full max-sm:w-full max-sm:justify-center max-sm:flex-col max-sm:gap-0 max-sm:bg-white max-sm:px-0':burger,
-        'max-sm:hidden': !burger}">
-            <NuxtLink to = "/" class="my auto p-2 hover:bg-gray-500 rounded-lg max-sm:border-b-2 max-sm:w-full max-sm:border-black max-sm:text-center">Home</NuxtLink>
-            <div class="ml-auto p-2 hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center max-sm:px-0 max-sm:pb-0" @click = "switch_submenu">LABS
-                <div class="flex flex-col top-2 absolute bg-white text-black w- text-center max-sm:relative max-sm:w-full max-sm:bg-red-100 max-sm:px-0" v-show="submenu">
-                    <NuxtLink to =/lab4 class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">LAB4</NuxtLink>
-                    <NuxtLink to =/lab5 class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">LAB5</NuxtLink>
-                    <NuxtLink to =/lab6 class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">LAB6</NuxtLink>
-                </div>ы
+        
+        <nav :class="burger 
+            ? 'basis-1/2 flex flex-col absolute top-full left-0 w-full bg-white px-0 max-sm:flex'
+            : 'basis-1/2 flex flex-row items-center justify-end px-8 gap-4 max-sm:hidden'">
+            <NuxtLink to="/" class="p-2 hover:bg-gray-500 rounded-lg max-sm:border-b-2 max-sm:w-full max-sm:border-black max-sm:text-center">Home</NuxtLink>
+            
+            <div class="relative">
+                <div class="p-2 cursor-pointer hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center" @click="switch_submenu">LABS</div>
+                <div v-show="submenu" class="absolute left-0 top-full w-full bg-white shadow-md max-sm:relative">
+                    <NuxtLink to="/lab4" class="block p-2 border-b hover:bg-gray-500 hover:text-white">LAB4</NuxtLink>
+                    <NuxtLink to="/lab5" class="block p-2 border-b hover:bg-gray-500 hover:text-white">LAB5</NuxtLink>
+                    <NuxtLink to="/lab6" class="block p-2 border-b hover:bg-gray-500 hover:text-white">LAB6</NuxtLink>
+                </div>
             </div>
-            <NuxtLink to = "/login" class="{'my auto p-2 hover:bg-gray-500 rounded-lg max-sm:border-b-2 max-sm:w-full max-sm:border-black max-sm:text-center': submenu,
-            'my-auto p-2 hover-bg-gray-500 hover:text-white max-sm:w-full max-sm:border-b-2 max-sm:text-center':!submenu}">Login</NuxtLink>
-            <NuxtLink to = "/logout" class="my auto p-2 hover:bg-gray-500 rounded-lg max-sm:border-b-2 max-sm:w-full max-sm:border-black max-sm:text-center">Logout</NuxtLink>
+
+            <NuxtLink to="/login" class="p-2 hover:bg-gray-500 rounded-lg max-sm:border-b-2 max-sm:w-full max-sm:border-black max-sm:text-center">Login</NuxtLink>
+            <NuxtLink to="/logout" class="p-2 hover:bg-gray-500 rounded-lg max-sm:border-b-2 max-sm:w-full max-sm:border-black max-sm:text-center">Logout</NuxtLink>
         </nav>
-        <div v-if="!burger" @click="switch_burger" class = "max-sm:flex-col max-sm:flex max-sm:mr-8 max-sm:justify-between max-sm:items-center max-sm:h6">
-            <span class="h-[3px] w-full bg-brown-200"></span>
-            <span class="h-[3px] w-full bg-brown-200"></span>
-            <span class="h-[3px] w-full bg-brown-200"></span>
-        </div>
-        <div v-else class = "max-sm:flex-col max-sm:flex max-sm:mr-8 max-sm:justify-between max-sm:items-center max-sm:h6" @click="switch_burger">
-            <span class="h-[3px] w-full bg-brown-200 rotate-45 relative top[9px]"></span>
-            <span class="h-[3px] w-full bg-brown-200 opacity-0"></span>
-            <span class="h-[3px] w-full bg-brown-200 relative bottom-3 rotate-[-45deg]"></span>
+
+        <!-- Кнопка бургера -->
+        <div @click="switch_burger" class="max-sm:flex flex-col justify-center items-center space-y-1 p-2 cursor-pointer">
+            <span class="h-[3px] w-6 bg-gray-700 transition-transform" :class="burger ? 'rotate-45 translate-y-[6px]' : ''"></span>
+            <span class="h-[3px] w-6 bg-gray-700 transition-opacity" :class="burger ? 'opacity-0' : ''"></span>
+            <span class="h-[3px] w-6 bg-gray-700 transition-transform" :class="burger ? '-rotate-45 -translate-y-[6px]' : ''"></span>
         </div>
     </header>
-    <main class="p-5 flex bg-black-300 h-screen">
+
+    <main class="p-5 flex bg-gray-300 h-screen">
         <slot />
     </main>
-    <footer class="flex flex-row bg-gray-75 border-t-2 bourder-gray-100 item-center justify-center px-6 py-2 gap-3">
-        <a href="https://www.youtube.com/" class="hover:sepia hover:brightness-125 hover:contrast-150"><img src="assets/images/youtube.svg" class="h-16 w-16 "></a>
-        <a href="https:/github.com/"><img src="assets/images/github.svg" class="h-16 w-16 transition duration-300 hover:grayscale"></a>
-        <a href="https:/www.facebook.com/"><img src="assets/images/facebook.svg" class="h-16 w-16"></a>
-        <a href="https://steamcommunity.com/"><img src="assets/images/steam.svg" class="h-16 w-16"></a>
+
+    <footer class="flex flex-row bg-gray-75 border-t-2 border-gray-100 items-center justify-center px-6 py-2 gap-3">
+        <a href="https://www.youtube.com/" class="hover:sepia hover:brightness-125 hover:contrast-150"><img src="assets/images/youtube.svg" class="h-16 w-16" alt="YouTube"></a>
+        <a href="https://github.com/"><img src="assets/images/github.svg" class="h-16 w-16 transition duration-300 hover:grayscale" alt="GitHub"></a>
+        <a href="https://www.facebook.com/"><img src="assets/images/facebook.svg" class="h-16 w-16" alt="Facebook"></a>
+        <a href="https://steamcommunity.com/"><img src="assets/images/steam.svg" class="h-16 w-16" alt="Steam"></a>
     </footer>
 </template>
 
 <script setup lang="ts">
-import {useHead} from '#app';
-import {ref} from 'vue';
+import { ref } from 'vue';
+import { useHead } from '#app';
 
 useHead({
-    script:[
+    script: [
         {
             async: true,
             src: 'https://www.googletagmanager.com/gtag/js?id=G-GMRMMGH1FY'
         },
         {
-            innerHTML:`
+            innerHTML: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-GMRMMGH1FY');
             `,
-            type:'text/javascript',
+            type: 'text/javascript',
         },
     ],
-    _dangerouslyDisableSanitizersByTagID:{
-        gtag:['innerHTML'],
+    _dangerouslyDisableSanitizersByTagID: {
+        gtag: ['innerHTML'],
     },
 });
 
-const burger = ref<boolean>(false)
-const submenu = ref<boolean>(false)
-const switch_burger =()=>(burger.value = !burger.value)
-const switch_submenu =()=>(submenu.value = !submenu.value)
+const burger = ref<boolean>(false);
+const submenu = ref<boolean>(false);
 
+const switch_burger = () => {
+    burger.value = !burger.value;
+    if (!burger.value) submenu.value = false; // Закрываем подменю при закрытии бургера
+};
+
+const switch_submenu = () => {
+    submenu.value = !submenu.value;
+};
 </script>
